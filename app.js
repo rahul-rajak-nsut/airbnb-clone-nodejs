73555804f26a935b889eb03582b56f0bded17959
rahul-rajak-nsut <rahulrajak.cs@gmail.com>
2026-08-25 23:02:34 +0530

    changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
-const Review=require("./models/review.js");
+
 
 //routers
 const listingRouter = require("./routes/listing.js");
